4d19c9f0aba91a55eee56fbcd231d163fe656e4d
ali.abukahil <ali.abukahil@gmail.com>
2022-06-30 16:23:40 +0200

    Intersection Observer API Navbar done

--- 3-project-NavbarEffect/app.js
+++ 3-project-NavbarEffect/app.js
@@ -10,8 +10,28 @@ let observer = new IntersectionObserver(navScroll, options);
 
 function navScroll(entries) {
   entries.forEach((entry) => {
-    console.log(entry);
+    // console.log(entry);
     const className = entry.target.className;
+    // selecting the data-page Attribute in html nav sec.
+    const activeLink = document.querySelector(`[data-page="${className}"]`);
+    // selecting the data-index Attribute in html main sec.
+    const elementIndex = entry.target.getAttribute("data-index");
+    // selecting th coordinates in the Inter section Observer Entry object API
+    const coordinates = activeLink.getBoundingClientRect();
+    const directions = {
+      height: coordinates.height,
+      width: coordinates.width,
+      top: coordinates.top,
+      left: coordinates.left,
+    };
+
+    if (entry.isIntersecting) {
+      trans.style.setProperty("height", `${directions.height}px`);
+      trans.style.setProperty("width", `${directions.width}px`);
+      trans.style.setProperty("top", `${directions.top}px`);
+      trans.style.setProperty("left", `${directions.left}px`);
+      trans.style.backgroundColor = gradients[elementIndex];
+    }
   });
 }
 
