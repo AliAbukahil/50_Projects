a5d8e565289de389beac26fe251be16f4a5ddf20
ali.abukahil <ali.abukahil@gmail.com>
2022-07-03 12:26:56 +0200

    Arranging Images Next to Each Other

---
+++ 10-project-GalleryCarousel/app.js
@@ -0,0 +1,15 @@
+const list = document.querySelector(".list");
+let imgs = Array.from(list.children);
+const nextBtn = document.querySelector(".btn-right");
+const prevBtn = document.querySelector(".btn-left");
+
+// Getting images width
+const imgWidth = imgs[0].getBoundingClientRect().width;
+//console.log(imgWidth);
+
+// Arranging imgs next to each other
+function setImgPosition(img, index) {
+  img.style.left = imgWidth * index + "px";
+}
+
+imgs.forEach(setImgPosition);
