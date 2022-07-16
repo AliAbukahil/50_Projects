f94d159f7090ae650f2c0b0df27fa32be349f131
ali.abukahil <ali.abukahil@gmail.com>
2022-07-16 22:19:26 +0200

    Creating Event Listeners for the Enter Key & Plus Icon

--- 19-project-TheToDoListApp/app.js
+++ 19-project-TheToDoListApp/app.js
@@ -6,15 +6,21 @@ const toDoAddBtn = document.querySelector(".fa-plus-circle");
 
 // Selecting the Icons class names
 const checkBtn = "fa-check-circle";
-const UncheckBtn = "fa-circle-btn";
+const UncheckBtn = "fa-circle-thin";
 const textLineThrough = "line-through";
+//-------------------------------------------------------------
+
+//-------------------------------------------------------------
 
 // To Do Container
 let toDoContainer = [];
 let id = 0;
 
+//-------------------------------------------------------------
 // addToDo Function
 function addToDo(toDo, id, done, trash) {
+  if (trash === true) return;
+
   const toDoDone = done ? checkBtn : UncheckBtn;
   const toDoLine = done ? textLineThrough : "";
   const item = `
@@ -25,4 +31,45 @@ function addToDo(toDo, id, done, trash) {
     </li>
   `;
   // https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentElement
+  const toDoItemPosition = "beforeend";
+  toDoList.insertAdjacentHTML(toDoItemPosition, item);
+}
+
+// addToDo("Buy Coffee", 0, false, false);
+// addToDo("Buy Coffee", 0, true, false);
+// addToDo("Buy Coffee", 0, true, true);
+
+//-------------------------------------------------------------
+
+// Adding a to-do to the list when the enter Key is pressed
+
+document.addEventListener("keyup", displayToDo);
+
+// Adding a to-do to the list when the + icon is clicked
+
+toDoAddBtn.addEventListener("click", displayToDo);
+
+// DisplayToDo Function
+
+function displayToDo(event) {
+  // 13 is the keycode for enter
+  if (
+    event.keyCode === 13 ||
+    event.target.classList.value === "fa fa-plus-circle"
+  ) {
+    const toDo = input.value;
+    // Checking whether the input field is NOT empty
+    if (toDo) {
+      addToDo(toDo, id, false, false);
+      toDoContainer.push({
+        name: toDo,
+        id: id,
+        done: false,
+        trash: false,
+      });
+      id++;
+    }
+
+    input.value = "";
+  }
 }
