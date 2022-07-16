d94e44ad2ec1c3b4c0a22a6c2dda2e232b4452db
ali.abukahil <ali.abukahil@gmail.com>
2022-07-16 16:51:18 +0200

    3. Creating an addToDo Function Part 2

--- 19-project-TheToDoListApp/app.js
+++ 19-project-TheToDoListApp/app.js
@@ -9,8 +9,20 @@ const checkBtn = "fa-check-circle";
 const UncheckBtn = "fa-circle-btn";
 const textLineThrough = "line-through";
 
+// To Do Container
+let toDoContainer = [];
+let id = 0;
+
 // addToDo Function
 function addToDo(toDo, id, done, trash) {
   const toDoDone = done ? checkBtn : UncheckBtn;
   const toDoLine = done ? textLineThrough : "";
+  const item = `
+    <li class="item">
+      <i class="fa ${toDoDone} complete" status="complete" id="${id}"></i>
+      <p class="text ${toDoLine}">${toDo}</p>
+      <i  class="fa fa-trash-o delete" status="delete" id="${id}"></i>
+    </li>
+  `;
+  // https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentElement
 }
