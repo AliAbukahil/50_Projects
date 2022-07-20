b528ea486565c6dae6f24a9ba5f53e416ecc5d06
ali.abukahil <ali.abukahil@gmail.com>
2022-07-20 20:43:22 +0200

    Creating the dragstart and dragend Event Listeners

---
+++ 21-project-SimpleDrag&Drop/app.js
@@ -0,0 +1,23 @@
+const lists = document.querySelectorAll(".list");
+const listItems = document.querySelectorAll(".list-item");
+
+let draggedItem = null;
+
+for (let a = 0; a < listItems.length; a++) {
+  const item = listItems[a];
+  console.log(item);
+
+  item.addEventListener("dragstart", function () {
+    draggedItem = item;
+    setTimeout(function () {
+      item.style.display = "none";
+    }, 50);
+  });
+
+  item.addEventListener("dragend", function () {
+    setTimeout(function () {
+      item.style.display = "block";
+      draggedItem = null;
+    }, 50);
+  });
+}
