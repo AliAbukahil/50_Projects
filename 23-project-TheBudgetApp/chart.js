fe8d5070d481faedbef435b594c3ba9b0563913b
ali.abukahil <ali.abukahil@gmail.com>
2022-07-27 00:39:14 +0200

    Creating the Chart P1

---
+++ 23-project-TheBudgetApp/chart.js
@@ -0,0 +1,29 @@
+const chart = document.querySelector(".chart");
+const canvas = document.createElement("canvas");
+
+// Dimensions
+canvas.height = 120;
+canvas.width = 120;
+
+///
+chart.appendChild(canvas);
+
+// Initializing a 2d context
+const context2D = canvas.getContext("2d");
+
+context2D.lineWidth = 10;
+
+const radius = 50;
+
+// drawCircle Function
+function drawCircle(color, ratio, anticlockwise) {
+  context2D.strokeStyle = color;
+  context2D.beginPath();
+  context2D.arc(60, 60, radius, 0, ratio * Math.PI * 2, anticlockwise);
+  context2D.stroke();
+}
+
+// drawCircle("red", 0.5, false);
+// drawCircle("green", 0.25, true);
+
+function updateChart() {}
