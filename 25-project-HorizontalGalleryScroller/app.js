07f170fa11929fb26287eeba8a0c9c8d46a740af
ali.abukahil <ali.abukahil@gmail.com>
2022-07-28 20:24:13 +0200

    Scrolling the Gallery on mouseMove

--- 25-project-HorizontalGalleryScroller/app.js
+++ 25-project-HorizontalGalleryScroller/app.js
@@ -1,14 +1,17 @@
 const slider = document.querySelector(".container");
 let isDown = false;
 let startX;
-let scrollLeft;
+let scrollToLeft;
 
 slider.addEventListener("mousedown", (e) => {
   isDown = true;
   slider.classList.add("active");
 
+  // https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/offsetLeft
   startX = e.pageX - slider.offsetLeft;
-  console.log(startX);
+
+  //https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollLeft
+  scrollToLeft = slider.scrollLeft;
 });
 
 slider.addEventListener("mouseup", (e) => {
@@ -19,4 +22,15 @@ slider.addEventListener("mouseup", (e) => {
 slider.addEventListener("mouseleave", (e) => {
   isDown = false;
   slider.classList.remove("active");
+});
+
+slider.addEventListener("mousemove", (e) => {
+  // !isDown = if isDown is false
+  if (!isDown) return;
+  e.preventDefault();
+
+  const distanceX = e.pageX - slider.offsetLeft;
+  // console.log({ distanceX, startX });
+  const walk = distanceX - startX;
+  slider.scrollLeft = scrollToLeft - walk;
 });
