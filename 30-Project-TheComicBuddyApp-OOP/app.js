0523c0cac5c687be537c9c54d32046a1fd883d9b
ali.abukahil <ali.abukahil@gmail.com>
2022-08-24 17:28:45 +0200

    creating the addSuperhero Function

--- 30-Project-TheComicBuddyApp-OOP/app.js
+++ 30-Project-TheComicBuddyApp-OOP/app.js
@@ -10,10 +10,24 @@ class SuperheroEntry {
 }
 
 // creating the superhero List Class
-class superheroList {}
+class SuperheroList {
+  addSuperhero(entry) {
+    const listData = document.querySelector(".superhero-list-data");
+    // CREATING AN HTML ELEMENT
+    const listContainer = document.createElement("ul");
+    listContainer.setAttribute("id", "list");
+
+    listContainer.innerHTML += `
+    <li>${entry.superheroName}</li>
+    <li>${entry.superheroUniverse}</li>
+    <li>${entry.superheroPower}</li>
+    <i class="fas fa-trash"></i>
+    `;
+  }
+}
 
 // ***************** Events ***************
-const form = document.querySelector("superhero-form");
+const form = document.querySelector(".superhero-form");
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
@@ -21,6 +35,19 @@ form.addEventListener("submit", function (e) {
   let [superheroName, superheroUniverse, superheroPower] = [
     document.querySelector("#name").value,
     document.querySelector("#universe").value,
-    Document.querySelector("#power").value,
+    document.querySelector("#power").value,
   ];
+
+  // Instantiating the SuperheroEntry class
+  const entry = new SuperheroEntry(
+    superheroName,
+    superheroUniverse,
+    superheroPower
+  );
+
+  // Instantiating the SuperheroList class
+  const list = new SuperheroList();
+
+  list.addSuperhero(entry);
+  console.log(list);
 });
