2a06f9ca1a53f86dab572b31f1b4b6b0d36da0d1
ali.abukahil <ali.abukahil@gmail.com>
2022-09-13 11:58:58 +0200

    Creating The Vertical Velocity and Randomizing The Speed, Size and Location

--- 40-project-Random-Stars/app.js
+++ 40-project-Random-Stars/app.js
@@ -10,12 +10,32 @@ const size = 0.005; // maximum star size as a fraction of the screen width
 const speed = 0.05; // fraction of screen width per second
 
 // Setting up the stars
+let stars = [];
+let starsSpeed = speed * canvasEl.width;
+let horizontalVelocity = starsSpeed * randomSign() * Math.random();
+let verticalVelocity =
+  Math.sqrt(Math.pow(starsSpeed, 2) - Math.pow(horizontalVelocity, 2)) *
+  randomSign();
+
+// randomizing the stars speed, size & Location
+for (let i = 0; i < starsNumber; i++) {
+  let speedBoost = Math.random() * 2.5 + 0.5;
+  stars[i] = {
+    starRadius: (Math.random() * size * canvasEl.width) / 2,
+    horizontalPosition: Math.floor(Math.random() * canvasEl.width),
+    verticalPosition: Math.floor(Math.random() * canvasEl.hight),
+    horizontalVelocity: horizontalVelocity * speedBoost,
+    verticalVelocity: horizontalVelocity * speedBoost,
+  };
+}
 
 //--------------------- The Game Loop
-let timeDiff,
-  timeLast = 0;
 
 // the animation loop
+let timeDiff,
+  timeLast = 0;
+requestAnimationFrame(runStars);
+
 function runStars(timeNow) {
   // calculate the frame difference
   timeDiff = timeNow - timeLast;
