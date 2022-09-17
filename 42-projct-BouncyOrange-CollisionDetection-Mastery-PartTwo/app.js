6d4285646c946e045c2e53d7e246d9bdf62ef8b6
ali.abukahil <ali.abukahil@gmail.com>
2022-09-17 19:32:56 +0200

    creating The Loop

--- 42-projct-BouncyOrange-CollisionDetection-Mastery-PartTwo/app.js
+++ 42-projct-BouncyOrange-CollisionDetection-Mastery-PartTwo/app.js
@@ -7,7 +7,7 @@ let xP, yP; // x and y Axis position
 let xV, yV; // x and y Axis Velocity
 
 xP = canvasEl.width / 2;
-xP = canvasEl.height / 2;
+yP = canvasEl.height / 2;
 
 xV = Math.floor(Math.random() * 201 + 99) / FPS;
 yV = Math.floor(Math.random() * 201 + 99) / FPS;
@@ -20,3 +20,24 @@ if (Math.floor(Math.random() * 2) === 0) {
 if (Math.floor(Math.random() * 2) === 0) {
   yV = -yV;
 }
+
+// The Game Loop
+function runGame() {
+  // Moving the ball from its current position
+  xP += xV;
+  yP += yV;
+
+  // clearing the canvas
+  canvasContext.clearRect(0, 0, canvasEl.width, canvasEl.height);
+
+  // Drawing the ball
+  canvasContext.beginPath();
+  canvasContext.fillStyle = "orangered";
+  //canvasContext.strokeStyle = "black";
+  canvasContext.arc(xP, yP, radius, 0, Math.PI * 2);
+  canvasContext.fill();
+  // canvasContext.stroke();
+}
+
+// set interval
+setInterval(runGame, 1000 / FPS);
