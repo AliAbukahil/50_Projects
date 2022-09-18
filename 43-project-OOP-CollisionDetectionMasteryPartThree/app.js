80ef06c2f7c8a3a5c1a6d4ab8766e398799e08cf
ali.abukahil <ali.abukahil@gmail.com>
2022-09-18 15:41:50 +0200

    creating The Balls

--- 43-project-OOP-CollisionDetectionMasteryPartThree/app.js
+++ 43-project-OOP-CollisionDetectionMasteryPartThree/app.js
@@ -4,6 +4,21 @@ const canvasContext = canvasEl.getContext("2d");
 canvasEl.width = window.innerWidth;
 canvasEl.height = window.innerHeight;
 
+let mouseEffect = {
+  x: undefined,
+  y: undefined,
+};
+
+let maxiRadius = 50;
+let miniRadius = 5;
+
+// mouse move event listener
+window.addEventListener("mousemove", (e) => {
+  mouseEffect.x = e.x;
+  mouseEffect.y = e.y;
+  console.log(mouseEffect);
+});
+
 // RunBalls Class
 class RunBalls {
   constructor(xP, yP, xV, yV, radius, red, green, blue) {
@@ -39,13 +54,26 @@ class RunBalls {
     this.xP += this.xV;
     this.yP += this.yV;
 
+    // mouse move effect
+    if (
+      mouseEffect.x - this.xP < 20 &&
+      mouseEffect.x - this.xP > -20 &&
+      mouseEffect.y - this.yP < 20 &&
+      mouseEffect.y - this.yP > -20
+    ) {
+      if (this.radius < maxiRadius) {
+        this.radius += 1;
+      }
+    } else if (this.radius > miniRadius) {
+      this.radius -= 1;
+    }
     this.drawBalls();
   }
 }
 
 // balls Array
 let ballsArray = [];
-for (let i = 0; i < 500; i++) {
+for (let i = 0; i < 1000; i++) {
   let radius = 30;
   let xP = Math.random() * (canvasEl.width - radius * 2) + radius;
   let yP = Math.random() * (canvasEl.height - radius * 2) + radius;
@@ -58,7 +86,7 @@ for (let i = 0; i < 500; i++) {
   ballsArray.push(new RunBalls(xP, yP, xV, yV, radius, red, green, blue));
 }
 
-console.log(ballsArray);
+// console.log(ballsArray);
 
 // The Game Loop
 function animateBalls() {
