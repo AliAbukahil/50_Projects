cde93d9cf8407127b724dc659a9635fa43bc71ea
ali.abukahil <ali.abukahil@gmail.com>
2022-09-29 12:36:49 +0200

    Moving the Paddle

--- 46 project-TheBreakoutGame-OOP-AdvancedLevel-Fully-Responsive/app.js
+++ 46 project-TheBreakoutGame-OOP-AdvancedLevel-Fully-Responsive/app.js
@@ -29,6 +29,10 @@ let width, height, wall;
 // initializing the paddle, ball classes
 let paddle, ball, touchX; // touch location
 
+// Arrow keys Events
+document.addEventListener("keydown", keydown);
+document.addEventListener("keyup", keyup);
+
 // *-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-* Resize Window Event *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-**-*-*- //
 window.addEventListener("resize", setDimensions);
 
@@ -36,6 +40,9 @@ window.addEventListener("resize", setDimensions);
 function playGame() {
   requestAnimationFrame(playGame);
 
+  // update Functions
+  updatePaddle();
+  // draw Functions
   drawBackground();
   drawWalls();
   drawPaddle();
@@ -56,6 +63,42 @@ function drawPaddle() {
     paddle.w,
     paddle.h
   );
+}
+
+// *-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-* ArrowKeys Functions *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-**-*-*- //
+function keyDown(e) {
+  switch (e.keyCode) {
+    case 37: // left arrow -> move arrow to left
+      movePaddle(DIRECTIONS.LEFT);
+      break;
+    case 39: // Right arrow -> move paddle to Right
+      movePaddle(DIRECTIONS.RIGHT);
+      break;
+  }
+}
+
+function keyUp(e) {
+  switch (e.keyCode) {
+    case 37:
+    case 39:
+      movePaddle(DIRECTIONS.STOP);
+      break;
+  }
+}
+
+// *-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-* MovePaddle Function *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-**-*-*- //
+function movePaddle(direction) {
+  switch (direction) {
+    case DIRECTIONS.LEFT:
+      paddle.xV = -paddle.speed;
+      break;
+    case DIRECTIONS.RIGHT:
+      paddle.xV = paddle.speed;
+      break;
+    case DIRECTIONS.STOP:
+      paddle.xV = 0;
+      break;
+  }
 }
 
 // *-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-* DrawWalls Function *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-**-*-*- //
@@ -86,7 +129,22 @@ function setDimensions() {
   canvasEl.height = height;
 }
 
-// the Paddle class
+// *-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-* updatePaddle class  *-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-* //
+function updatePaddle() {
+  // move the paddle
+  // let lastPaddleX = paddle.x;
+  paddle.x += (paddle.xV / 1000) * 20;
+
+  // wall collision detection for paddle
+
+  if (paddle.x < wall + paddle.w / 2) {
+    paddle.x = wall + paddle.w / 2;
+  } else if (paddle.x > canvasEl.width - wall - paddle.w / 2) {
+    paddle.x = canvasEl.width - wall - paddle.w / 2;
+  }
+}
+
+// *-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-* the Paddle class  *-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-* //
 class Paddle {
   constructor(paddleWidth, paddleHeight, paddleSpeed) {
     this.w = paddleWidth * width;
